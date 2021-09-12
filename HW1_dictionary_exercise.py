f9f384d05072d186e199f2720d35bf258b7ffd63
sydneycheng <89552503+sydneycheng@users.noreply.github.com>
2021-09-12 17:12:26 -0500

    Add files via upload

--- HW1_dictionary_exercise.py
+++ HW1_dictionary_exercise.py
@@ -1,16 +1,16 @@
 
 def main():
-    # course_info = {
-    #             'CS101':[3004,'Haynes','8:00 a.m.'],
-    #             'CS102':[4501,'Alvarado','9:00 a.m.'],
-    #             'CS103':[6755,'Rich','10:00 a.m.'],
-    #             'NT110':[1244,'Burke','11:00 a.m.'],
-    #             'CM241':[1411,'Lee','1:00 p.m.']
-    #             }
+    course_info = {
+                'CS101':[3004,'Haynes','8:00 a.m.'],
+                'CS102':[4501,'Alvarado','9:00 a.m.'],
+                'CS103':[6755,'Rich','10:00 a.m.'],
+                'NT110':[1244,'Burke','11:00 a.m.'],
+                'CM241':[1411,'Lee','1:00 p.m.']
+                }
 
-    # course_num = input("Enter a course number: ")
+    course_num = input("Enter a course number: ")
 
-    # print(course_info[course_num])
+    print(course_info[course_num])
 
 
     codes = {
